dace1c5c4167d1cfd80ab6fb58eefedda2fdd0f5
Relt <31004181+rekter@users.noreply.github.com>
2019-04-23 20:53:01 +0300

    Update DevMode

--- js/devMode.js
+++ js/devMode.js
@@ -4,7 +4,6 @@ var succpass                    = 13373006;
 function passwordType(pass, succpass) {
     if (pass == succpass) {
         alert('Access success');
-        window.location.replace('devMode.html')
     } else if (pass != succpass) {
         alert('Access denied');
         window.location.replace('index.html');
